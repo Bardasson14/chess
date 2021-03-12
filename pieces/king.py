3533f53ba39ed35741a4904729c6a7ab0de28b30
Vitor <vbardasson@id.uff.br>
2021-03-12 11:59:25 -0300

    Refatoração nos códigos de peão, peça e rei

--- pieces/king.py
+++ pieces/king.py
@@ -29,34 +29,34 @@ class King(Piece):
     
     def movD(self, coord, matrix):
         if(coord[1]!=7 and coord[0]!=0): #limite superior e lateral direito 
-            fr=matrix[(coord[0]-1,coord[1]+1)]['piece']#↗↗↗
+            fr = matrix[(coord[0]-1,coord[1]+1)]['piece']#↗↗↗
             if ((fr and fr.color!=self.color) or (not fr)):
                 self.possibleMoves.append((coord[0]-1,coord[1]+1))
 
         if(coord[1]!=0 and coord[0]!=0):  #limite superior e lateral esquerdo
-            fl=matrix[(coord[0]-1,coord[1]-1)]['piece']#↖↖↖
+            fl = matrix[(coord[0]-1,coord[1]-1)]['piece']#↖↖↖
             if((fl and fl.color!=self.color) or (not fl)) :
                 self.possibleMoves.append((coord[0]-1,coord[1]-1))
 
         if(coord[1]!=7 and coord[0]!=7): #limite inferior e lateral direito
-            br=matrix[(coord[0]+1,coord[1]+1)]['piece']#↘↘↘
+            br = matrix[(coord[0]+1,coord[1]+1)]['piece']#↘↘↘
             if(br and br.color!=self.color or (not br)):
                 self.possibleMoves.append((coord[0]+1,coord[1]+1))
 
         if(coord[1]!=0 and coord[0]!=7): #limite inferior e lateral esquerdo
-            bl=matrix[(coord[0]+1,coord[1]-1)]['piece']#↙↙↙
+            bl = matrix[(coord[0]+1,coord[1]-1)]['piece']#↙↙↙
             if((bl and bl.color!=self.color) or (not bl)):
                 self.possibleMoves.append((coord[0]+1,coord[1]-1))
 
     def movV(self, coord, matrix): 
         if((coord[0]-1>=0)):    #limite superior
-            f=matrix[(coord[0]-1,coord[1])]['piece']    #⬆⬆⬆
+            f = matrix[(coord[0]-1,coord[1])]['piece']    #⬆⬆⬆
             if ((f and f.color!=self.color) or (not f)):
                 self.possibleMoves.append((coord[0]-1,coord[1]))
                 
         if(coord[0]+1<=7): #limite inferior
-            b=matrix[(coord[0]+1,coord[1])]['piece']    #⬇⬇⬇
-            if (b!=None and b.color!=self.color or (not b)):
+            b = matrix[(coord[0]+1,coord[1])]['piece']    #⬇⬇⬇
+            if (b and b.color!=self.color or (not b)):
                 self.possibleMoves.append((coord[0]+1,coord[1]))
                 self.possibleMoves.append((coord[0]+1,coord[1]))
     
